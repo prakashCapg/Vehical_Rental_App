8c8f08df950c1bb94231d969740c571880f9f695
KRISHNA CHAITANYA <yenimetla-venkata.krishna-chaitanya@capgemini.com>
2024-09-16 22:48:08 +0530

    updated popup and inputfield component

--- src/pages/preview/index.js
+++ src/pages/preview/index.js
@@ -1,17 +1,28 @@
 import React from "react";
 import Popup from "../../components/PopUp/Popup";
-import InputField from "../../components/InputField/InputField";
+import InputFieldText from "../../components/InputField_Text/InputField_text";
 import Tabs from "../../components/Tabs";
 import Buttons from "../../components/button/Buttons";
+import InputFieldDate from "../../components/InputField_Date/InputField_Date";
 
 const Preview = () => {
   const tabs = ["Car", "Bike", "6 Seater"];
   return (
     <div>
       <h1>Preview</h1>
+
       <Popup />
-      <InputField />
+
+      <InputFieldText label="Enter text or digits:">
+        <p>Please input letters or numbers only.</p>
+      </InputFieldText>
+
+      <InputFieldDate label="Select a date:">
+        <p>Please pick a date from the calendar.</p>
+      </InputFieldDate>
+
       <Tabs tabs={tabs} />
+
       <Buttons></Buttons>
     </div>
   );
