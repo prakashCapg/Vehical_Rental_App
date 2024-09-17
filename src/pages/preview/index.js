75a7f0b27c0a55666a2f5c7accba53520175895b
KRISHNA CHAITANYA <yenimetla-venkata.krishna-chaitanya@capgemini.com>
2024-09-17 10:20:48 +0530

    updated popup

--- src/pages/preview/index.js
+++ src/pages/preview/index.js
@@ -3,13 +3,11 @@ import Popup from "../../components/PopUp/Popup";
 import InputFieldText from "../../components/InputField_Text/InputField_text";
 import Tabs from "../../components/Tabs";
 import Buttons from "../../components/button/Buttons";
-<<<<<<< HEAD
 import InputFieldDate from "../../components/InputField_Date/InputField_Date";
-=======
+
 import ImageUpload from "../../components/ImageUpload/Index";
 import Dropdown from "../../components/Dropdown/Dropdown";
 import Card1 from "../../components/Card1/Card1";
->>>>>>> 03036c109bc59366c8d2ae9563555ec7f26ff894
 
 const Preview = () => {
   const tabs = ["Car", "Bike", "6 Seater"];
@@ -28,13 +26,11 @@ const Preview = () => {
       </InputFieldDate>
 
       <Tabs tabs={tabs} />
-<<<<<<< HEAD
 
-=======
-      <ImageUpload />
->>>>>>> 03036c109bc59366c8d2ae9563555ec7f26ff894
       <Buttons></Buttons>
+
       <Dropdown></Dropdown>
+
       <Card1></Card1>
     </div>
   );
