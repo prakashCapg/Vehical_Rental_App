1cb7d7f851894cd57ba45c154a912b4603d6aff7
B Bharath Kumar Reddy <b-bharath-kumar.reddy@capgemini.com>
2024-09-24 18:14:47 +0530

    Created add vehicle page UI, Input Field Text and validations, and single select dropdown components

--- src/components/InputField_Text/InputField_text.js
+++ src/components/InputField_Text/InputField_text.js
@@ -1,14 +1,13 @@
 import React, { useState } from "react";
+import "./InputField_text.css";
 
 const InputField = ({ label, children }) => {
   const [value, setValue] = useState("");
   const [error, setError] = useState("");
-  const [submittedValue, setSubmittedValue] = useState(null);
 
   const validateInput = (inputValue) => {
-    const isNumber = /^[0-9]*$/.test(inputValue);
-    const isText = /^[A-Za-z]*$/.test(inputValue);
-    return isText || isNumber;
+    const regex = /^[a-zA-Z0-9]*$/.test(inputValue);
+    return regex;
   };
 
   const handleChange = (e) => {
@@ -17,59 +16,22 @@ const InputField = ({ label, children }) => {
       setValue(inputValue);
       setError("");
     } else {
-      setError("Invalid input: only letters or digits are allowed.");
-    }
-  };
-
-  const handleSubmit = (e) => {
-    e.preventDefault(); // Prevent page reload
-    if (value) {
-      setSubmittedValue(value);
-      alert(`Submitted Value: ${value}`);
-      setValue(""); // Clear the input field after submission
-    } else {
-      setError("Please enter a valid value before submitting.");
+      setError("Invalid input: only letters and digits are allowed.");
     }
   };
 
   return (
-    <div style={{ margin: "20px" }}>
-      <form onSubmit={handleSubmit}>
-        <label style={{ display: "block", marginBottom: "10px" }}>
-          {label}
-        </label>
+    <div>
+      <div>
+        <label>{label}</label>
         <input
           type="text"
           value={value}
           onChange={handleChange}
-          style={{
-            padding: "10px",
-            width: "300px",
-            border: "1px solid #ccc",
-            borderRadius: "4px",
-          }}
+          className="Input-field-text"
         />
         {error && <p style={{ color: "red", marginTop: "5px" }}>{error}</p>}
-        <button
-          type="submit"
-          style={{
-            padding: "10px 20px",
-            backgroundColor: "blue",
-            color: "white",
-            border: "none",
-            borderRadius: "5px",
-            fontSize: "16px",
-          }}
-        >
-          Submit
-        </button>
-      </form>
-      {submittedValue && (
-        <p style={{ marginTop: "10px" }}>
-          {/*<strong>Submitted Value:</strong> {submittedValue}*/}
-        </p>
-      )}
-      {children}
+      </div>
     </div>
   );
 };
