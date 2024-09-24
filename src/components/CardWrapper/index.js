30e68dc8a7acf8517fb04f0b4c39cd5e8e22cd92
Afroj <shaik.afroj@capgemini.com>
2024-09-24 14:58:45 +0530

    updated cardwrapper

--- src/components/CardWrapper/index.js
+++ src/components/CardWrapper/index.js
@@ -11,7 +11,7 @@ const CardWrapper = ({ cardData }) => {
 
   return (
     <div className="scrollbar scrollbar-thumb-yellow-400 scrollbar-track-sky-300 overflow-y-scroll">
-      <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4  w-full sm:w-auto">
+      <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-3  w-full sm:w-auto">
         {cardData.map((card, index) => (
           <Card2
             key={index}
