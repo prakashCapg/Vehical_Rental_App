0342fdc97ec46f2ed21cb2ad23d83f4f3721b155
Afroj <shaik.afroj@capgemini.com>
2024-09-24 17:26:40 +0530

    updated cardwrapper and tabs and vehiclelist

--- src/components/CardWrapper/index.js
+++ src/components/CardWrapper/index.js
@@ -10,22 +10,21 @@ const CardWrapper = ({ cardData }) => {
   };
 
   return (
-    <div className="scrollbar scrollbar-thumb-yellow-400 scrollbar-track-sky-300 overflow-y-scroll">
-      <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-3  w-full sm:w-auto">
-        {cardData.map((card, index) => (
-          <Card2
-            key={index}
-            image={card.image}
-            title={card.title}
-            price={card.price}
-            description={card.description}
-            features={card.features}
-            onClick={() => {
-              handlebutton(card.id);
-            }}
-          />
-        ))}
-      </div>
+    <div
+      className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-3 w-full sm:w-auto overflow-y-auto"
+      style={{ maxHeight: "80vh" }} // Adjust the maxHeight as needed
+    >
+      {cardData.map((card, index) => (
+        <Card2
+          key={index}
+          image={card.image}
+          title={card.title}
+          price={card.price}
+          description={card.description}
+          features={card.features}
+          onClick={() => handlebutton(card.id)}
+        />
+      ))}
     </div>
   );
 };
