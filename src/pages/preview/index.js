f682cefa433c8481341cdeda9000282d33d3b080
AVINASH710-SIDAM <sidam.avinash@capgemini.com>
2024-09-25 13:18:55 +0530

    header updated

--- src/pages/preview/index.js
+++ src/pages/preview/index.js
@@ -11,8 +11,10 @@ import InputFieldDate from "../../components/InputField_Date/InputField_Date";
 import ImageUpload from "../../components/ImageUpload/Index";
 import Dropdown from "../../components/Dropdown/Dropdown";
 import Card1 from "../../components/Card1/Card1";
+import { Link } from 'react-router-dom';
 
-const Preview = () => {
+
+const Preview = ({ menuItems }) => {
 
   const cardData=[
     {id:1,image:img,title:'BMW',price:'800',description: "A reliable compact car with excellent fuel efficiency.",features: ["Automatic", "Air Conditioning", "Bluetooth", "Backup Camera"]},
@@ -35,7 +37,16 @@ const Preview = () => {
   return (
     <div>
       <h1>Preview</h1>
+      <h2>Menu Items:</h2>
+            <ul>
+                {menuItems.map((item, index) => (
+                    <li key={index}>
+                        <Link to={item.path}>{item.label}</Link>
+                    </li>
+                ))}
+            </ul>
 
+   
       <Popup />
 
       <InputFieldText label="Enter text or digits:">
