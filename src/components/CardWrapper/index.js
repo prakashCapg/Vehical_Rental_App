2ab5fc52e23248a3bdfaece3209c8809fc4548ef
KRISHNA CHAITANYA <yenimetla-venkata.krishna-chaitanya@capgemini.com>
2024-10-09 15:52:18 +0530

    modified errors

--- src/components/CardWrapper/index.js
+++ src/components/CardWrapper/index.js
@@ -2,28 +2,30 @@ import React from "react";
 import Card2 from "../Card2";
 import { useNavigate } from "react-router-dom";
 
-const CardWrapper = ({ cardData, onCardClick }) => {
+const CardWrapper = ({ cardData }) => {
+  const navigate = useNavigate();
+
+  const handlebutton = (id) => {
+    navigate(`/vehicle/${id}`);
+  };
+
   return (
     <div
       className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-3 w-full sm:w-auto overflow-y-auto"
       style={{
-
-        maxHeight: "80vh", // Adjust the maxHeight as needed
-        backgroundColor: "white", // Set the background color to white
-
+        maxHeight: "80vh",
+        backgroundColor: "white",
       }}
     >
-      {cardData.map((card) => (
+      {cardData.map((card, index) => (
         <Card2
-
           key={index}
-
           imagePath={card.imagePath}
           category={card.category}
           rentPricePerHour={card.rentPricePerHour}
           description={card.description}
           features={card.features}
-          onClick={() => onCardClick(card.VehicleId)}
+          onClick={() => handlebutton(card.id)}
         />
       ))}
     </div>
--- src/pages/preview/index.js
+++ src/pages/preview/index.js
@@ -1,19 +1,32 @@
-
-
+import React from "react";
+import PopUp from "../../components/PopUp/Popup";
+import img from "./img1.jpg";
+import InputFieldText from "../../components/InputField_Text/InputField_text";
+import Tabs from "../../components/Tabs";
+import TestButton from "../../components/Buttons/Buttons";
+import TestCard1 from "./TestCard1";
+import TestDropdown from "./TestDropdown";
+import Popup from "../../components/PopUp/Popup";
 import BookingHistory from "../BookingHistory/BookingHistory";
 import InputFieldDate from "../../components/InputField_Date/InputField_Date";
 import ImageUpload from "../../components/ImageUpload/Index";
 import Dropdown from "../../components/Dropdown/Dropdown";
 import Card1 from "../../components/Card1/Card1";
+import InputField_Search from "../../components/InputField_Search";
+import Calendar from "../../components/Calendar";
+import Accordion from "../../components/Accordion/Accordion";
+import Workflow from "../../components/Workflow/index";
+import Tracking from "../BookingHistory/Tracking";
+import Buttons from "../../components/Buttons/Buttons";
 
-
+const Preview = () => {
+  const tabs = ["Car", "Bike", "6 Seater"];
 
   return (
     <div className="min-h-screen">
       <h1>Preview</h1>
 
-
-
+      <PopUp />
       {/*<LoginPopup />*/}
 
       <Workflow />
@@ -29,8 +42,9 @@ import Card1 from "../../components/Card1/Card1";
       </InputFieldDate>
 
       <ImageUpload />
-
-
+      <TestButton></TestButton>
+      {/* <TestCard1></TestCard1> */}
+      <TestDropdown></TestDropdown>
 
       {/* <InputField_Search
         apiEndPoint="http://localhost:3002/bookings"
@@ -39,10 +53,8 @@ import Card1 from "../../components/Card1/Card1";
       {/* <BookingHistory /> */}
 
       <Accordion />
-
       <Tracking />
       <Calendar />
-
     </div>
   );
 };
